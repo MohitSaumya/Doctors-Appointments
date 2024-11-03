f216f419e1260c3789650156a4b8dcba18706d66
MohitSaumya <mohitsingh2124@gmail.com>
2024-11-03 22:52:10 +0530

    Please

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Home />
+       <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/doctors' element={<Doctors />} />
+      <Route path='/doctors/:speciality' element={<Doctors />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/about' element={<About />}/>
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/my-profile' element={<MyProfile />} />
+      <Route path='/my-appointments' element={<MyAppointments />} />
+      <Route path='/appointment/:docId' element={<Appointment />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
